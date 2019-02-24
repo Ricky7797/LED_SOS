547b595d27060b22f6cf9a6d7cb3a6cfebb0dd82
Ricky7797 <47427820+Ricky7797@users.noreply.github.com>
2019-02-24 16:30:05 -0500

    Add files via upload

---
+++ sketch_feb24b.ino
@@ -0,0 +1,31 @@
+int in1 = 6;
+
+
+void setup() {
+  pinMode(in1, OUTPUT);
+}
+
+void loop() {
+  Blink(6,300);
+  Blink(6,300);
+  Blink(6,300);
+
+  Blink(6,1000);
+  Blink(6,1000);
+  Blink(6,1000);
+
+  Blink(6,300);
+  Blink(6,300);
+  Blink(6,300);
+
+  delay(2000);
+}
+
+
+
+void Blink(int x, int y){
+  digitalWrite(x, LOW);
+  delay(y);
+  digitalWrite(x, HIGH);
+  delay(y);
+}
